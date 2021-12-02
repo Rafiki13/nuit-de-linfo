2069e41357bfe895b454660e11fda1fe4be88c07
Rafiki13 <58651523+Rafiki13@users.noreply.github.com>
2021-12-03 00:51:20 +0100

    frontend v0.4

--- Frontend/src/app/soumission/soumettre/soumettre.component.ts
+++ Frontend/src/app/soumission/soumettre/soumettre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-soumettre',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoumettreComponent implements OnInit {
 
+  @Input() type: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  newTypeOfEntry(){
+    console.log(this.type);
   }
 
 }
