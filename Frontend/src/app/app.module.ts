6e4db714e446545b9b42c1a791e8624f0832e6cf
Rafiki13 <58651523+Rafiki13@users.noreply.github.com>
2021-12-02 19:55:27 +0100

    frontend v0.2
    j'ai UN PEU avancé

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,12 +7,24 @@ import { AccueilComponent } from './accueil/accueil/accueil.component';
 import { PageRechercheComponent } from './recherche/page-recherche/page-recherche.component';
 import { BarreDeRechercheComponent } from './recherche/barre-de-recherche/barre-de-recherche.component';
 import { MissionComponent } from './mission/mission/mission.component';
-import { SoumettreComponent } from './soumissions/soumettre/soumettre.component';
+import { SoumettreComponent } from './soumission/soumettre/soumettre.component';
 import { GestionComponent } from './soumission/gestion/gestion.component';
 import { LoginComponent } from './users/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
 import { MenuComponent } from './root/menu/menu.component';
 import { FooterComponent } from './root/footer/footer.component';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: "", component: AccueilComponent},
+  {path: "recherche", component: PageRechercheComponent},
+  {path: "mission", component: MissionComponent},
+  {path: "soumission/ajout", component: SoumettreComponent},
+  {path: "soumission/gestion", component: GestionComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent}
+]
 
 @NgModule({
   declarations: [
@@ -30,7 +42,8 @@ import { FooterComponent } from './root/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
